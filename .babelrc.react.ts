007df8799ebdfe16a8bd4f4dbbba10534a9bb450
Andrew Wei <andrewscwei@gmail.com>
2020-05-08 18:52:15 -0700

    Add dotfiles

---
+++ .babelrc.react.ts
@@ -0,0 +1,30 @@
+{
+  "env": {
+    "development": {
+      "plugins": [
+        "babel-plugin-styled-components",
+        "react-hot-loader/babel"
+      ]
+    }
+  },
+  "retainLines": true,
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "browsers": "last 2 versions"
+        }
+      }
+    ],
+    "@babel/preset-typescript",
+    "@babel/preset-react"
+  ],
+  "plugins": [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-optional-chaining",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["babel-plugin-styled-components", { "displayName": false }]
+  ]
+}
