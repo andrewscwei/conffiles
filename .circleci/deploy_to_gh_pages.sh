3ac584175deb88a2ec7ad202d0b16d35ca85dc25
Andrew Wei <andrewscwei@gmail.com>
2020-05-10 19:56:04 -0700

    Add manual Netlify deploy script

--- .circleci/deploy_to_gh_pages.sh
+++ .circleci/deploy_to_gh_pages.sh
@@ -4,9 +4,9 @@
 # - GH_USER: Name of GitHub user to use for the commit
 # - GH_PAGES_DIR: Directory (relative to project root) to deploy to GitHub Pages
 
-__GH_PAGES_DIR__=${GH_PAGES_DIR:-.gh-pages}
-__GH_USER__=${GH_USER:-$CIRCLE_PROJECT_USERNAME}
-__ORIGIN_URL__=`git config --get remote.origin.url`
+GH_PAGES_DIR=${GH_PAGES_DIR:-.gh-pages}
+GH_USER=${GH_USER:-$CIRCLE_PROJECT_USERNAME}
+ORIGIN_URL=`git config --get remote.origin.url`
 
 if [ `git branch | grep gh-pages` ]; then
   git branch -D gh-pages
@@ -16,15 +16,15 @@ git checkout -b gh-pages
 npm run pages
 
 # Move generated files to root and delete everything else.
-find . -maxdepth 1 ! -name '.' ! -name '..' ! -name ${__GH_PAGES_DIR__} ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv ${__GH_PAGES_DIR__}/* .
-rm -R ${__GH_PAGES_DIR__}/
+find . -maxdepth 1 ! -name '.' ! -name '..' ! -name ${GH_PAGES_DIR} ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv ${GH_PAGES_DIR}/* .
+rm -R ${GH_PAGES_DIR}/
 
 # Push to gh-pages.
-git config user.name "$__GH_USER__"
-git config user.email "$__GH_USER__@users.noreply.github.com"
+git config user.name "$GH_USER"
+git config user.email "$GH_USER@users.noreply.github.com"
 git add -fA
 git commit --allow-empty -m "[SKIP CI] $(git log -1 --pretty=%B)"
-git push -f $__ORIGIN_URL__ gh-pages
+git push -f $ORIGIN_URL gh-pages
 
 echo "Successfully published docs to GitHub Pages"
---
+++ .github/workflows/deploy_to_netlify.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Required environment variables:
+# - GH_USER: Name of GitHub user to use for the commit
+# - NETLIFY_KEY: API key for Netlify
+
+source .circleci/get_opts.sh
+
+NETLIFY_API=https://api.netlify.com/api/v1
+NETLIFY_SITE_ID=""
+GH_USER=${GH_USER:-$CIRCLE_PROJECT_USERNAME}
+ORIGIN_URL=`git config --get remote.origin.url`
+PACKAGE_NAME=$(cat package.json | jq -r ".name")
+PACKAGE_VERSION=$(cat package.json | jq -r ".version")
+PACKAGE_FILE=$PACKAGE_NAME-$PACKAGE_VERSION
+
+if [ -f .circleci/netlify ]; then
+  NETLIFY_SITE_ID=$(cat .circleci/netlify)
+
+  echo "Existing site ID ($NETLIFY_SITE_ID) found in repo, verifying that site exists..."
+  echo
+
+  RESULT=$(curl -H "User-Agent: $GH_USER ($GH_USER@users.noreply.github.com)" $NETLIFY_API/sites/$NETLIFY_SITE_ID?access_token=$NETLIFY_KEY)
+
+  echo
+  echo $RESULT
+  echo
+
+  __RESPONSE_CODE__=$(echo $RESULT | jq -r ".code")
+
+  # Site not found.
+  if [[ $__RESPONSE_CODE__ == "404" ]]; then
+    NETLIFY_SITE_ID=""
+  fi
+fi
+
+if [[ $NETLIFY_SITE_ID == "" ]]; then
+  echo "No netlify file detected or site is not found, creating a new site on Netlify..."
+  echo
+
+  RESULT=$(curl -H "Content-Type: application/zip" -H "Authorization: Bearer $NETLIFY_KEY" --data-binary "@package/$PACKAGE_FILE.zip" $NETLIFY_API/sites)
+  NETLIFY_SITE_ID=$(echo $RESULT | jq -r ".id")
+  SITE_NAME=$(echo $RESULT | jq -r ".subdomain")
+  SITE_URL=https://$SITE_NAME.netlify.com
+
+  echo "$NETLIFY_SITE_ID" > .circleci/netlify
+
+  git config user.name "$GH_USER"
+  git config user.email "$GH_USER@users.noreply.github.com"
+  git add ./.circleci/netlify
+  git commit -m "[Skip CI] Adding generated netlify file"
+  git push -f $ORIGIN_URL HEAD:master
+
+  echo
+  echo "Done! Your site URL is:"
+  echo $SITE_URL
+else
+  echo "Deploying to site ${NETLIFY_SITE_ID}..."
+  echo
+
+  RESULT=$(curl -H "Content-Type: application/zip" -H "Authorization: Bearer $NETLIFY_KEY" --data-binary "@package/$PACKAGE_FILE.zip" $NETLIFY_API/sites/$NETLIFY_SITE_ID/deploys)
+  SITE_URL=$(echo $RESULT | jq -r ".url")
+
+  echo
+  echo $RESULT
+  echo
+  echo "Done! Your site URL is:"
+  echo $SITE_URL
+fi
